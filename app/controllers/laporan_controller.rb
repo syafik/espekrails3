949d7da6155b00ec1a74c59b7bb226856e3b9b8a
Hilman Zaky <hilman@hilmans-girlfriend.(none)>
2013-02-17 17:02:31 +0700

    All Laporan in Senarai Laporan

--- app/controllers/laporan_controller.rb
+++ app/controllers/laporan_controller.rb
@@ -2,14 +2,14 @@
 class LaporanController < ApplicationController
   #  	require "pdf/writer"
   #  	require "pdf/simpletable"
-	layout "standard-layout"
+  layout "standard-layout"
 
-	def initialize
-    @states = State.find(:all, :order=>"description")
+  def initialize
+    @states = State.find(:all, :order => "description")
     @genders = Gender.all
-    @races = Race.find(:all, :order=>"id")
+    @races = Race.find(:all, :order => "id")
     @profile_statuses = ProfileStatus.all
-    @religions = Religion.find(:all, :order=>"id")
+    @religions = Religion.find(:all, :order => "id")
     @countries = Country.all
     @marital_statuses = MaritalStatus.all
     @places = Place.find(:all, :conditions => "place_type_id = '2'")
@@ -17,130 +17,131 @@ class LaporanController < ApplicationController
     @cert_levels = CertLevel.all
     @majors = Major.all
     @job_profiles = JobProfile.all
-    @titles = Title.find(:all, :order=>"description")
+    @titles = Title.find(:all, :order => "description")
     @course_departments = CourseDepartment.all
     #@courses = Course.all
     #@course_implementations = CourseImplementation.all
-	end
+    super
+  end
 
 
   def index
     list_laporan
-  	render :action=>'list_laporan'
+    render :action => 'list_laporan'
   end
-  
+
   def list_laporan
   end
-  
+
   def mohon_espek
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		p = dmy(params[:report1],"/","-")
-  		q = dmy(params[:report2],"/","-")
-  		@students = CourseApplication.find(:all, :conditions=>"date_apply BETWEEN '#{p}' AND '#{q}'")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      p = dmy(@report_date_from, "/", "-")
+      q = dmy(@report_date_to, "/", "-")
+      @students = CourseApplication.find(:all, :conditions => "date_apply BETWEEN '#{p}' AND '#{q}'")
     else
       @students = []
     end
   end
-  
+
   def statistik_kursus_tarikh
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		@p = dmy(params[:report1],"/","-")
-  		@q = dmy(params[:report2],"/","-")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      @p = dmy(@report_date_from, "/", "-")
+      @q = dmy(@report_date_to, "/", "-")
     else
-      @p = '2008-01-01'
-      @q = '2008-01-01'
+      @p = '2013-01-01'
+      @q = '2013-01-01'
     end
   end
 
   def pencapaian_ujian
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		@p = dmy(params[:report1],"/","-")
-  		@q = dmy(params[:report2],"/","-")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      @p = dmy(@report_date_from, "/", "-")
+      @q = dmy(@report_date_to, "/", "-")
     else
-      @p = '2008-01-01'
-      @q = '2008-01-01'
+      @p = '2013-01-01'
+      @q = '2013-01-01'
     end
-  
+
   end
-  
+
   def pencapaian_ujian_bidang
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		@p = dmy(params[:report1],"/","-")
-  		@q = dmy(params[:report2],"/","-")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      @p = dmy(@report_date_from, "/", "-")
+      @q = dmy(@report_date_to, "/", "-")
     else
-      @p = '2008-01-01'
-      @q = '2008-01-01'
+      @p = '2013-01-01'
+      @q = '2013-01-01'
     end
   end
 
   def penilaian_penceramah
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		@p = dmy(params[:report1],"/","-")
-  		@q = dmy(params[:report2],"/","-")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      @p = dmy(@report_date_from, "/", "-")
+      @q = dmy(@report_date_to, "/", "-")
     else
-      @p = '2008-01-01'
-      @q = '2008-01-01'
+      @p = '2013-01-01'
+      @q = '2013-01-01'
     end
   end
 
-  
+
   def penilaian_kemudahan
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		@p = dmy(params[:report1],"/","-")
-  		@q = dmy(params[:report2],"/","-")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      @p = dmy(@report_date_from, "/", "-")
+      @q = dmy(@report_date_to, "/", "-")
     else
-      @p = '2008-01-01'
-      @q = '2008-01-01'
+      @p = '2013-01-01'
+      @q = '2013-01-01'
     end
   end
-  
+
   def penilaian_penyelarasan
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		@p = dmy(params[:report1],"/","-")
-  		@q = dmy(params[:report2],"/","-")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      @p = dmy(@report_date_from, "/", "-")
+      @q = dmy(@report_date_to, "/", "-")
     else
-      @p = '2008-01-01'
-      @q = '2008-01-01'
+      @p = '2013-01-01'
+      @q = '2013-01-01'
     end
   end
 
   def penilaian_isi_kandungan
-  	penilaian_penyelarasan
+    penilaian_penyelarasan
   end
 
   def senarai_ulasan
-  	penilaian_penyelarasan
+    penilaian_penyelarasan
   end
 
   def kutipan_yuran
-  	penilaian_penyelarasan
+    penilaian_penyelarasan
   end
-  
+
   def kutipan_yuran_bhg
-  	penilaian_penyelarasan
+    penilaian_penyelarasan
   end
 
   def status_perlaksanaan
-  	@course_implementation = CourseImplementation.find(params[:id])
-  	@status_perlaksanaan = StatusPerlaksanaan.find_by_course_implementation_id(@course_implementation.id)
+    @course_implementation = CourseImplementation.find(params[:id])
+    @status_perlaksanaan = StatusPerlaksanaan.find_by_course_implementation_id(@course_implementation.id)
     @check = @status_perlaksanaan
     render :layout => "standard-layout"
   end
-  
+
   def sp_create
     20.times do |n|
       y = n + 1
@@ -156,7 +157,7 @@ class LaporanController < ApplicationController
       redirect_to :action => 'status_perlaksanaan', :id => params[:status_perlaksanaan][:course_implementation_id]
     end
   end
-  
+
   def sp_update
     @status_perlaksanaan = StatusPerlaksanaan.find_by_course_implementation_id(params[:id])
     for q in @status_perlaksanaan
@@ -178,23 +179,23 @@ class LaporanController < ApplicationController
   end
 
   def audit_trail
-  	if (params[:report1] != nil) && (params[:report2] != nil)
-  		@p = dmy(params[:report1],"/","-")
-  		@q = dmy(params[:report2],"/","-")
-      @report_date_from = params[:report1]
-      @report_date_to = params[:report2]
+    if (params[:report1] != nil) && (params[:report2] != nil)
+      @report_date_from = params[:report1].first
+      @report_date_to = params[:report2].first
+      @p = dmy(@report_date_from, "/", "-")
+      @q = dmy(@report_date_to, "/", "-")
     else
-      @p = @q = dmy(Time.now.to_formatted_s(:my_format_4),"/","-")
+      @p = @q = dmy(Time.now.to_formatted_s(:my_format_4), "/", "-")
     end
-    @audit_trails = AuditTrail.find(:all, :conditions=>"created_at BETWEEN '#{@p}' AND '#{@q}'" , :order=>"created_on DESC")
+    @audit_trails = AuditTrail.find(:all, :conditions => "created_at BETWEEN '#{@p}' AND '#{@q}'", :order => "created_on DESC")
   end
-  
-	private
-	def dmy(ymd_date, separator, new_separator)
-		a = ymd_date.to_s.split(separator)
-		b = "#{a[2]}#{new_separator}#{a[1]}#{new_separator}#{a[0]}"
-		return b
-	end
+
+  private
+  def dmy(ymd_date, separator, new_separator)
+    a = ymd_date.to_s.split(separator)
+    b = "#{a[2]}#{new_separator}#{a[1]}#{new_separator}#{a[0]}"
+    return b
+  end
 
 
 end
