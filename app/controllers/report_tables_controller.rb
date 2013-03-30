f13416494f84569a1e403af776eaa085316a2a03
Rikki Yanhari <rikki.yanhari@kiranatama.com>
2013-03-30 15:04:23 +0700

    report table updated

--- app/controllers/report_tables_controller.rb
+++ app/controllers/report_tables_controller.rb
@@ -6,26 +6,26 @@ class ReportTablesController < ApplicationController
  
     if params[:month_from].present? && params[:month_until].present? && params[:year].present?
       filter = "AND EXTRACT(month FROM date_apply) >= #{params[:month_from]} AND EXTRACT(month FROM date_apply) <= #{params[:month_until]} AND EXTRACT(year FROM date_apply) = #{params[:year]}"
+      filter2 = "AND EXTRACT(month FROM ci.date_apply_start) >= #{params[:month_from]} AND EXTRACT(month FROM ci.date_apply_start) <= #{params[:month_until]} AND EXTRACT(year FROM ci.date_apply_start) = #{params[:year]}"
+
+      sql = "select ci.code as code, c.name as course_name, (
+        select count(*)
+        FROM vw_detailed_applicants_all vdaa
+        WHERE vdaa.course_implementation_id = ci.id	AND student_status_id IN (4,5,6,8,9,7,2) #{filter}) as pemohon,
+        (select count(*)
+        FROM vw_detailed_applicants_all vdaa
+        WHERE vdaa.course_implementation_id = ci.id	AND student_status_id IN (2) #{filter}) as dipilih,
+        (select count(*)
+        FROM vw_detailed_applicants_all vdaa
+        WHERE vdaa.course_implementation_id = ci.id	AND student_status_id IN (5,8,9) #{filter}) as hadir
+      FROM course_implementations ci, courses c
+      WHERE ci.course_id = c.id #{filter2}
+        "
+      @reports =  CourseApplication.find_by_sql(sql)
     else
       flash[:notice] = "sila isi filter dengan lengkap" if params[:month_from].present? || params[:month_until].present? || params[:year].present?
-      filter = ""
+      @reports =  []
     end
-
-    sql = "select ci.code as code, c.name as course_name, (
-      select count(*)
-      FROM vw_detailed_applicants_all vdaa
-      WHERE vdaa.course_implementation_id = ci.id	AND student_status_id IN (4,5,6,8,9,7,2) #{filter}) as pemohon,
-      (select count(*)
-      FROM vw_detailed_applicants_all vdaa
-      WHERE vdaa.course_implementation_id = ci.id	AND student_status_id IN (2) #{filter}) as dipilih,
-      (select count(*)
-      FROM vw_detailed_applicants_all vdaa
-      WHERE vdaa.course_implementation_id = ci.id	AND student_status_id IN (5,8,9) #{filter}) as hadir
-    FROM course_implementations ci, courses c
-    WHERE ci.course_id = c.id
-    "
-
     #@reports =  CourseApplication.paginate_by_sql(sql, :page => params[:page], :per_page => 20 )
-    @reports =  CourseApplication.find_by_sql(sql)
   end
 end
