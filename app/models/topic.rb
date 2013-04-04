24a0b801f5d8d5f92c36c27140027c0f564e573b
don <don@don-N43SL.(none)>
2013-04-04 10:53:26 +0700

    add attr accessor

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,11 +1,13 @@
 # -*- encoding : utf-8 -*-
 class Topic < ActiveRecord::Base
-    belongs_to :course_implementation
+  belongs_to :course_implementation
 	belongs_to :trainer
     	
 	has_many :evaluation_trainer_rankings
-	
-	validates_presence_of :title
+
+  attr_accessor :shown_at
+  
+	validates :title, :presence => true
 	
 	def question_list
 		list = self.evaluation_trainer_rankings.size
