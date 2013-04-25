e2880de3130179d1f770728ee0eaa57e7cc997d5
pushjaw <pushjaw@nazar-ThinkPad-T410.(none)>
2013-04-25 13:44:27 +0700

    fix routes senarai pemohon

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,7 @@ InstunRails3::Application.routes.draw do
   match '/user_applications/tambah_update/:id' => 'user_applications#tambah_update', :via => [:put]
   match '/user_applications/tambah/:id' => 'user_applications#tambah', :via => [:put]
   match '/user_applications/show_attendance/:id' => 'user_applications#show_attendance', :via => [:get]
+
   resources :user_applications do
     collection do
       get 'applied'
@@ -520,7 +521,7 @@ InstunRails3::Application.routes.draw do
   match '/course_applications/user_daftar_create/:id' => 'course_applications#user_daftar_create', :via => [:post]
   match '/course_applications/edit_by_user/:id' => 'course_applications#edit_by_user', :via => [:get]
   match '/course_applications/update/:id' => 'course_applications#edit_by_user', :via => [:post]
-
+  match '/course_applications/user_cancel/:id' => 'course_applications#user_cancel', :via => [:get]
   resources :course_applications, :except => [ :destroy ] do
     get 'new'
     get 'all'
@@ -546,6 +547,7 @@ InstunRails3::Application.routes.draw do
       post 'create_peserta'
       post 'cetak_surat_iklan'
       get 'new_popup'
+      post 'reject_selected_with_reason'
     end
     member do
       post 'cetak_surat_tawaran'
